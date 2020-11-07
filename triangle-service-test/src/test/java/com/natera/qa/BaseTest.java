ac145f7cf77f2d881c2b671804296df2c7db0095
StumpKeeper <alexeymkr@gmail.com>
2020-11-07 15:02:47 +0300

    - initial project setup

---
+++ triangle-service-test/src/test/java/com/natera/qa/BaseTest.java
@@ -0,0 +1,12 @@
+package com.natera.qa;
+
+import org.testng.annotations.Test;
+
+public class BaseTest {
+
+    @Test
+    public void test1() {
+        assert true;
+        System.out.println("Test 1 - COMPLETE");
+    }
+}
