4914a94bb0e5c28c8d892f68a9d8c66ed4e90e99
StumpKeeper <alexeymkr@gmail.com>
2020-11-10 13:24:09 +0300

    - added unit-tests for triangle model methods

---
+++ triangle-service-test/src/test/java/com/natera/qa/unit/TriangleTest.java
@@ -0,0 +1,38 @@
+package com.natera.qa.unit;
+
+import com.natera.qa.triangle.model.Triangle;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void checkTriangleValidation() {
+        Triangle validTriangle = Triangle.builder().firstSide(10.5).secondSide(12.3).thirdSide(21.3).build();
+        Triangle invalidTriangle = Triangle.builder().firstSide(10.5).secondSide(20.3).thirdSide(40.1).build();
+
+        Assert.assertTrue(validTriangle.valid());
+        Assert.assertFalse(invalidTriangle.valid());
+    }
+
+    @Test
+    public void checkValidTriangleGeneration() {
+        Assert.assertTrue(Triangle.randomValid().valid());
+    }
+
+    @Test
+    public void checkTriangleAreaCalculation() {
+        Triangle validTriangle = Triangle.builder().firstSide(10.5).secondSide(12.3).thirdSide(21.3).build();
+        double validArea = 43.154712590283815;
+
+        Assert.assertEquals(validTriangle.area(), validArea);
+    }
+
+    @Test
+    public void checkTrianglePerimeterCalculation() {
+        Triangle validTriangle = Triangle.builder().firstSide(70.5).secondSide(32.3).thirdSide(83.7).build();
+        double validPerimeter = 186.5;
+
+        Assert.assertEquals(validTriangle.perimeter(), validPerimeter);
+    }
+}
