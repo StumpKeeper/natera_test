089aa7efbc1bdfd2b4492fe47b3b5f09189db420
StumpKeeper <alexeymkr@gmail.com>
2020-11-12 14:34:34 +0300

    - added getMissingTriangleTest (404 for missing triangle)

--- triangle-service-test/src/test/java/com/natera/qa/triangle/GetTriangleTest.java
+++ triangle-service-test/src/test/java/com/natera/qa/triangle/GetTriangleTest.java
@@ -4,6 +4,8 @@ import com.natera.qa.triangle.model.Triangle;
 import com.natera.qa.triangle.service.TriangleService;
 import org.testng.annotations.Test;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GetTriangleTest extends BaseTriangleTest {
@@ -15,5 +17,10 @@ public class GetTriangleTest extends BaseTriangleTest {
 
         assertThat(TriangleService.getTriangle(createdTriangle.getId()))
                 .isEqualTo(createdTriangle);
+    }
+
+    @Test
+    public void getMissingTriangleTest() {
+        TriangleService.getTriangle(UUID.randomUUID(), 404);
     }
 }
